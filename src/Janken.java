be8d69e857632e00cbb9695258e6591c17ca4532
Mike <244047558+promike17-code@users.noreply.github.com>
2025-11-26 03:40:06 +0900

    Update Janken.java

--- src/Janken.java
+++ src/Janken.java
@@ -1,23 +1,30 @@
+import java.util.Scanner;
+import java.util.Random;
+
 public class Janken {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+
         System.out.println("じゃんけんをしましょう");
         System.out.println("0:グー 1:チョキ 2:パー");
 
-        int player = new java.util.Scanner(System.in).nextInt();
-        int cpu = new java.util.Random().nextInt(3);
-        String[] hands = {"グー","チョキ","パー"};
-        System.out.println("あなた : "+hands[player]+" コンピューター: "+hands[cpu]);
+        int player = scanner.nextInt();
+        int cpu = random.nextInt(3);
 
-        if(player == cpu) {
+        String[] hands = {"グー","チョキ","パー"};
+        System.out.println("あなた: " + hands[player] + " コンピューター: " + hands[cpu]);
+
+        if(player == cpu){
             System.out.println("あいこです");
         } else if (
             (player == 0 && cpu == 1) ||
             (player == 1 && cpu == 2) ||
             (player == 2 && cpu == 0)
-        ) {
+        ){
             System.out.println("あなたの勝ち");
         } else {
             System.out.println("あなたの負け");
         }
-    } 
-} 
+    }
+}
