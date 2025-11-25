0ed8ae976af56dd742004a710207555728a54700
Mike <244047558+promike17-code@users.noreply.github.com>
2025-11-26 02:36:25 +0900

    Update Janken.java

--- src/Janken.java
+++ src/Janken.java
@@ -1,11 +1,10 @@
 public class Janken{ 
   public static void main(String[] args){  
     System.out.println("じゃんけんをしましょう");//入力画面でじゃんけんをしましょうの表示
-    
-    System.out.println("0:グー　1:チョキ　2:パー");//入力画面で0:グー　1:チョキ　2:パーの表示
+    System.out.println("0:グー　1:チョキ　2:パー");//入力画面で0:グー 1:チョキ 2:パーの表示
     int player=new java.util.Scanner(System.in).nextInt();//playerが数字を入力するためのコード
     int cpu=new java.util.Random().nextInt(3);//CPUが0~2の数字をランダムに表示できるようにする
-    String[] hands ={"グー","チョキ","パー"};/*playerの入力数字とCPUがランダムに出した数字にグー、チョキ、パーをつける  0=グー、1=チョキ、2=パー*/
+    String[] hands ={"グー","チョキ","パー"};/*playerの入力数字とCPUがランダムに出した数字にグー、チョキ、パーをつける 0=グー、1=チョキ、2=パー*/
     System.out.println("あなた : "+hands[player]+"  コンピューター : "+hands[cpu]);//playerが入力したじゃんけんの手とCPUが出したじゃんけんの手を画面に表示する。
      if(player==cpu){//あいこパターン
         System.out.println("あいこです");
